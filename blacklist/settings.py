898a649c6bda56ff978e7c4f9d3a940fb7d79fb9
Fabiano Ribeiro <fabianoribeiro.uniao@gmail.com>
2026-02-08 00:35:11 -0300

    crispy forms

--- blacklist/settings.py
+++ blacklist/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dividas',
     'crispy_forms',
     'crispy_bootstrap5',
+    'dividas',
+    
     
 ]
 
